3825f6d97b53265a7e0606161b8d646e65ca2922
Paulo Henrique <paulo.henrique.hunter@gmail.com>
2022-05-14 00:10:07 -0300

    Finaliza os exercícios do dia 1

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_do_dia/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_do_dia/script.js
@@ -1,18 +1,179 @@
-const a = 20
-const b = 12
-const c = 45
+const a = 60
+const b = 44
+const c = 22
 
-/* ===
+
+
+/* =================================== Primeiro Exercício ===============================/*
 /*console.log(a+b)
 console.log(a-b)
 console.log(a*b)
 console.log(a/b)
 console.log(a%b)*/
 
+
+
+/* =================================== Segundo Exercício ===============================*/
+/*
 if ( a > b){
     console.log('A é maior do que b')
 }
 
 else{
     console.log('B é maior do que a')
+}*/
+
+
+
+
+/* =================================== Terceiro Exercício ===============================*/
+
+/*if ( a > b && a > c){
+    console.log('A é o maior')
 }
+
+else if (b > a && b > c) {
+    consolse.log('B é o maior')
+}
+else if (c > a && c > b) {
+    consolse.log('C é o maior')
+}*/
+
+/* =================================== Quarto Exercício ===============================*/
+
+/* if ( a > 0){
+    console.log('A é positivo')
+}
+
+else if (a < 0){
+    console.log('A é negativo')
+}
+
+else(
+    console.log('A é zero')
+) */
+
+/* =================================== Quinto Exercício ===============================*/
+
+
+/*let triangle = null
+
+if ( a + b + c == 180){
+ triangle = true
+
+}
+
+else{
+    console.log(Error)
+}
+
+console.log(triangle)*/
+
+/* =================================== Sexto Exercício ===============================*/
+
+/*let piece = 'PAWN'
+
+switch(piece.toLocaleLowerCase()){
+    case 'bishop':
+        console.log('Diagonals')
+        break;
+    case 'queen':
+        console.log('Any directions for how many squares you desire')
+        break;
+    
+
+    case 'king':
+        console.log('Any directions for a single square')
+        break;
+    
+    case 'kight':
+        console.log('Moves in an L')
+        break;
+    
+    case 'rook':
+        console.log('In straight lines')
+        break;
+    
+    case 'pawn':
+        console.log('One square foward')
+        break;
+    
+
+}*/
+
+/* =================================== Sétimo Exercício ===============================*/
+
+/*let gradePercent = 78
+let gradeLetter = ''
+
+if( gradePercent >= 80 ){
+    gradeLetter = 'B'
+    
+} 
+else if( gradePercent >= 70 ){
+    gradeLetter = 'C'
+
+} 
+else if( gradePercent >= 60 ){
+    gradeLetter = 'D'
+
+} 
+else if( gradePercent >= 50 ){
+    gradeLetter = 'E'
+
+} 
+else if( gradePercent < 50 ){
+    gradeLetter = 'F'
+
+} 
+else if( gradePercent == 0 ){
+    gradeLetter = 'error'
+    
+
+} 
+
+console.log(gradeLetter)*/
+
+/* =================================== Oitavo Exercício ===============================*/
+
+/*if(a%2 == 0 || b%2 == 0 || c%2 == 0 ){
+    console.log('True')
+}
+
+else {
+    console.log('False')
+}*/
+/* =================================== Nono Exercício ===============================*/
+
+/*if(a%2 != 0 || b%2 != 0 || c%2 != 0 ){
+    console.log('True')
+}
+
+else {
+    console.log('False')
+} */
+
+/* =================================== Décimo Exercício ===============================*/
+
+/*const custoProduto = 20
+
+const valorDeVenda = 60
+
+let custoTotal = custoProduto + (custoProduto*0.20)
+
+
+console.log((valorDeVenda - custoTotal) 0* 1000)*/
+
+/* =================================== Décimo primeiro Exercício ===============================*/
+
+const salarioBruto = 3000
+
+let inss = salarioBruto -(salarioBruto*0.11)
+
+let impostoRenda =(  (inss*0.075)  -142.80)
+
+let salarioliquido =  inss - impostoRenda
+
+
+        console.log(salarioliquido)
+
