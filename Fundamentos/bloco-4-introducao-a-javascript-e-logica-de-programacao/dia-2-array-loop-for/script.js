5d7d812c841dbb1bae9d3714ab33723e6e64d67b
Paulo Henrique <paulo.henrique.hunter@gmail.com>
2022-05-14 15:25:12 -0300

    Finaliza os exercícios do segundo dia

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-loop-for/script.js
@@ -0,0 +1,136 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* ============================================ Primeiro Exercício =======================================*/
+/*
+for (let index = 0; index < numbers.length; index+=1){
+    console.log(numbers[index])
+}*/
+
+/* ============================================ Segundo Exercício =======================================*/
+
+
+/* let soma = null
+
+for (let index = 0; index < numbers.length; index+=1 ){
+   
+    soma  += numbers[index]
+}
+
+console.log(soma)*/
+
+/* ============================================ Terceiro Exercício =======================================*/
+
+/* let soma = null
+
+for (let index = 0; index < numbers.length; index+=1 ){
+   
+    soma  += numbers[index]
+}
+
+console.log(soma/numbers.length)*/
+
+/* ============================================ Quarto Exercício =======================================*/
+
+
+/*let soma = null
+
+for (let index = 0; index < numbers.length; index+=1 ){
+   
+    soma  += numbers[index]
+}
+
+let mediaAritmetica = soma/numbers.length
+
+
+if (mediaAritmetica > 20){
+    console.log("valor maior que 20")
+}
+
+else{
+    "valor menor ou igual a 20"
+}*/
+
+
+/* ============================================ Quinto Exercício =======================================*/
+
+
+
+/*let maiorNumero = null
+for( var index = 0; index < numbers.length; index +=1){ 
+    
+    if(maiorNumero < numbers[index]) {
+        
+        maiorNumero = numbers[index]; 
+    }
+
+}
+
+console.log(maiorNumero);*/
+
+
+
+
+/* ============================================ Sexto Exercício =======================================*/
+
+/*let oddNumbers = null
+
+for (let index = 0; index < numbers.length; index+=1 ){
+   
+    if(numbers[index]%2 != 0){
+        oddNumbers++
+    }
+    
+}
+
+if(oddNumbers != 0){
+    console.log(oddNumbers)
+}
+
+else {
+    console.log("nenhum valor ímpar encontrado")
+}*/
+
+
+/* ============================================ Sétimo Exercício =======================================*/
+
+
+/*let menorNumero = 1000
+for( var index = 0; index < numbers.length; index +=1){ 
+    
+    
+
+    if(menorNumero > numbers[index]) {
+        
+        menorNumero = numbers[index]; 
+    }
+
+}
+
+console.log(menorNumero);*/
+
+
+
+/* ============================================ Oitavo Exercício =======================================*/
+
+/*let array = [1]
+
+
+for (let index = 0; index < 24; index+=1 ){
+    array.push(1+array[index])
+
+
+}
+
+console.log(array)*
+
+/* ============================================ Nono Exercício =======================================*/
+
+/*let array = [1]
+
+
+for (let index = 0; index < 25; index+=1 ){
+    array.push(1+array[index])
+
+    console.log(array[index]/2)
+
+}*/
