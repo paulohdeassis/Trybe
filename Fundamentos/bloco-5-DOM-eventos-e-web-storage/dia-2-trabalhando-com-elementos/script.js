558d9ce9869154074e3f5307d749a25a52fa2a60
Paulo Henrique <paulo.henrique.hunter@gmail.com>
2022-06-02 19:17:17 -0300

    Finaliza os exercícios do segundo dia do bloco 5

--- Fundamentos/bloco-5-DOM-eventos-e-web-storage/dia-2-trabalhando-com-elementos/script.js
+++ Fundamentos/bloco-5-DOM-eventos-e-web-storage/dia-2-trabalhando-com-elementos/script.js
@@ -1,31 +1,9 @@
 
 
-/*Cada um dos exercícios é resolvido através de uma função que será chamada no início desse código*/
-
-newStringElement('h1', ' Exercício 5.2 - JavaScript DOM','body')
-
-createGenericElement('section', 'main-content','body')
-
-createGenericElement('section', 'center-content','.main-content')
-
-newStringElement('p','texto de teste, uhuulll','.center-content')
-
-createGenericElement('section', 'right-content','.main-content')
-    
-createGenericElement('section', 'left-content','.main-content')
-
-createImageElement( 'https://picsum.photos/200','.left-content', 'small-image')
-
-newStringElement('h3','Heading de teste','.main-content')
-newStringElement('h3','Heading de teste','.main-content')
-newStringElement('h3','Heading de teste','.main-content')
-createOrdedList(['Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez'],'.right-content','trybeList')
+/*Os exercícios são resolvidos através de funções que serão chamadas no final desse código*/
 
 
-
-   
-
-/* ============================= Exercícios 1 ,4 e 9 ====================================*/
+/* ============================= Exercícios 1 ,4 e 9 =========================*/
 function newStringElement(element, string, parent){
 
         let newElement = document.createElement(element)
@@ -40,7 +18,7 @@ function newStringElement(element, string, parent){
 }
 
     
-/* ============================= Exercícios 2, 3, 5 e 6 ====================================*/
+/* ============================= Exercícios 2, 3, 5 e 6 =====================*/
 function createGenericElement(tag, clas, parent){
 
         let elementType = document.createElement(tag)
@@ -84,4 +62,82 @@ function createOrdedList(values, parent, clas){
    }
 }
  
+/*============================== Exercício 9 e 10 =========================*/
 
+function addClassToAllOfType(element, clas){
+
+var tag = document.querySelectorAll(element)
+for (let index = 0; index < tag.length; index +=1){
+
+
+ tag[index].classList.add(clas)
+
+}
+
+}
+
+/*============================== Exercício 11 =============================*/
+function removeElement (parent, child){
+  let parentElement = document.querySelector(parent)
+  let removedElement = document.querySelector(child)
+
+  parentElement.removeChild(removedElement)
+
+}
+
+/*============================== Exercício 12 =============================*/
+function centralizeElement(element){
+        let centeredElement = document.querySelector(element)
+
+        centeredElement.style.marginRight = 'auto'
+}
+
+/*============================== Exercício 13 =============================*/
+
+function changeParentBackGround(element, color){
+
+ let elementParent = document.querySelector(element).parentElement
+
+ elementParent.style.backgroundColor = color
+}
+
+
+/*============================== Exercício 14 =============================*/
+
+function removeFromList(parent, index1, index2){
+ let elementParent = document.querySelector(parent)
+
+ let children = elementParent.childNodes;
+elementParent.removeChild(children[index1])
+elementParent.removeChild(children[index2])
+}
+
+
+
+
+
+newStringElement('h1', ' Exercício 5.2 - JavaScript DOM','body')
+
+createGenericElement('main', 'main-content','body')
+
+createGenericElement('section', 'center-content','.main-content')
+
+newStringElement('p','texto de teste, uhuulll','.center-content')
+
+createGenericElement('section', 'right-content','.main-content')
+    
+createGenericElement('section', 'left-content','.main-content')
+
+createImageElement( 'https://picsum.photos/200','.left-content', 'small-image')
+
+newStringElement('h3','Heading de teste','.main-content')
+newStringElement('h3','Heading de teste','.main-content')
+newStringElement('h3','Heading de teste','.main-content')
+createOrdedList(['Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez'],'.right-content','trybeList')
+
+addClassToAllOfType('h1', 'title')
+addClassToAllOfType('h3', 'description')
+removeElement('.main-content', '.left-content')
+centralizeElement('.right-content')
+changeParentBackGround('.center-content','#059862')
+removeFromList('.trybeList', 9, 8)
